7352628a5166937b8e633e1a64610ed18d61b749
darshan-pradhan <darshan-pradhan>
2022-12-01 08:50:22 +0000

    1st Commit

---
+++ Movies JSON/includes/configuration.py
@@ -0,0 +1,40 @@
+# Databricks notebook source
+# MAGIC 
+# MAGIC %md
+# MAGIC Define Data Paths.
+
+# COMMAND ----------
+
+# ANSWER
+username = "dpradhan"
+
+# COMMAND ----------
+
+
+classicPipelinePath = f"dbfs:/FileStore/tables/"
+landingPath = classicPipelinePath + "landing/"
+
+rawPath = classicPipelinePath + "raw/"
+bronzePath = classicPipelinePath + "bronze/"
+silverPath = classicPipelinePath + "silver/"
+silverQuarantinePath = classicPipelinePath + "silverQuarantine/"
+goldPath = classicPipelinePath + "gold/"
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC Configure Database
+
+# COMMAND ----------
+
+spark.sql(f"CREATE DATABASE IF NOT EXISTS movie_{username}")
+spark.sql(f"USE movie_{username}")
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC Import Utility Functions
+
+# COMMAND ----------
+
+# MAGIC %run ./utilities
